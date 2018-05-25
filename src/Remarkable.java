06a64e5ed19e11d062aace95a6efa4355ee63f86
1902051 <league@WTS14.attlocal.net>
2018-05-24 20:00:41 -0700

    I finally finished unbirthday and i'm on remarkable!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ src/Remarkable.java
@@ -0,0 +1,7 @@
+
+public class Remarkable {
+	public static void main(String[] args) {
+
+	}
+
+}
--- src/unbirthday.java
+++ src/unbirthday.java
@@ -2,16 +2,16 @@ import javax.swing.JOptionPane;
 
 public class unbirthday {
 	public static void main(String[] args) {
-		
-
 
 		String birthday = JOptionPane.showInputDialog("What is your birthday");
-if(birthday.equals("05/17"))
- JOptionPane.showMessageDialog(null,"HAPPY UNBIRTHDAY!!!"); 
-		
-	}
-	
-	
+		if (birthday.equals("05/17")) {
+			JOptionPane.showMessageDialog(null, "HAPPY BIRTHDAY!!!");
+		} else {
 
+			JOptionPane.showMessageDialog(null, "HAPPY UNBIRTHDAY!!! XD");
+
+		}
+
+	}
 
 }
