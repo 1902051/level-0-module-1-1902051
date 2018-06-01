050571e9a2f042cf64abbc8dabbff71fe6500c46
League <league@WTS13.attlocal.net>
2018-05-31 20:02:12 -0700

    I FINISHED REMARKABLE!!

--- src/Remarkable.java
+++ src/Remarkable.java
@@ -1,7 +1,37 @@
+import javax.swing.JOptionPane;
 
 public class Remarkable {
 	public static void main(String[] args) {
 
-	}
+		String orange = JOptionPane.showInputDialog("What is yo name");
 
+		if (orange.equals("Milan")) {
+			JOptionPane.showMessageDialog(null, "Well lets see here in the name dictanary...");
+			JOptionPane.showMessageDialog(null, "Milan made this program so I am awesome!");
+		} else if (orange.equals("Kai")) {
+			JOptionPane.showMessageDialog(null, "Well lets see here in the name dictanary...");
+			JOptionPane.showMessageDialog(null, "Kai is my friend and is really funny!");
+		}
+
+		else if (orange.equals("Mike")) {
+			JOptionPane.showMessageDialog(null, "Well lets see here in the name dictanary...");
+			JOptionPane.showMessageDialog(null, "He is our awesome teacher who is awesome.");
+		}
+
+		else if (orange.equals("Roshan")) {
+			JOptionPane.showMessageDialog(null, "Well lets see here in the name dictanary...");
+			JOptionPane.showMessageDialog(null, "He is meh awesome brother!!!");
+		}
+
+		else if (orange.equals("Tyler")) {
+			JOptionPane.showMessageDialog(null, "Well lets see here in the name dictanary...");
+			JOptionPane.showMessageDialog(null, "He is a dungoof!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+		}
+
+		else if (orange.equals("Michael")) {
+			JOptionPane.showMessageDialog(null, "Well lets see here in the name dictanary...");
+			JOptionPane.showMessageDialog(null, "Well... He exists?");
+		}
+
+	}
 }
