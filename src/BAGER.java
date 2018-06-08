96de52c3e726600e910692183d708887d950142b
1902051 <league@WTS10.attlocal.net>
2018-06-07 19:59:05 -0700

    Badger badger

---
+++ src/BAGER.java
@@ -0,0 +1,54 @@
+import javax.swing.JOptionPane;
+
+public class BAGER {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<5;i++) {
+		
+	
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mushroom, mushroom, a
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mush-mushroom, a
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	African! Snake, a snake
+		//	Snake! A snake, ooh it's a snake
+		//	It's a badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mushroom, mushroom, a
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mushroom, mushroom, a
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mushroom, mushroom, a
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mush-mushroom, a
+		//	Badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	African! Snake, a snake
+		//	Snake! A snake, ooh it's a snake
+		//	It's a badger, badger, badger, badger, badger, badger, badger, badger, badger, badger
+		//	Mushroom, mushroom, a
+
+			for(int e=0;e<10;e++) {
+
+			
+			
+			
+			for(int e=0;e<10;e++) {
+				
+				System.out.println("Badger");
+			}
+			for(int r=0;er<2;r++) {
+				System.out.println("Mushroom");
+
+			}
+			for(int m=0;m<1;m++) {
+				System.out.println("Snake!!!");
+
+			}
+			
+		}	
+	
+		}
+	
+	}
+}
